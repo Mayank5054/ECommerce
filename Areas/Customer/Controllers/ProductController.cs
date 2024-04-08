161595e373c4c43c1a14314633107a85c26196c9
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-08 12:44:21 +0530

    created Product controllers and view to display product create and retrive is done

---
+++ Areas/Customer/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
+namespace ECommerce.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductController :Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+          List<Product> data =   _db.Products.ToList();
+            return View(data);
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
