984cad48d17b85708e11883618c2a17e0bead109
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-09 10:45:27 +0530

    bug resolved :- New Category Created every time Product category changes

--- Areas/Customer/Controllers/ProductController.cs
+++ Areas/Customer/Controllers/ProductController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using ECommerce.Models;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 namespace ECommerce.Areas.Customer.Controllers
 {
     [Area("Customer")]
@@ -15,8 +18,8 @@ namespace ECommerce.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
-          List<Product> data =   _db.Products.ToList();
-            return View(data);
+            List<Product> data1 = _db.Products.Include(p => p.category).ToList();
+            return View(data1);
         }
         public IActionResult Create() {
             return View();
@@ -29,15 +32,38 @@ namespace ECommerce.Areas.Customer.Controllers
             return RedirectToAction("Index");
         }
         public IActionResult Edit(int id) {
-            Product data =  _db.Products.Find(id);
+
+            Product data = _db.Products.Include(_ => _.category).Where(p=>p.BookId==id).FirstOrDefault();
+            Console.WriteLine(data);
             return View("Update",data);
         }
 
         [HttpPost]
         public IActionResult Edit(Product product)
         {
-            _db.Products.Update(product);
-            _db.SaveChanges();
+
+            Product p1 = _db.Products.Include(_ => _.category)
+                .Where(_ => _.BookId == product.BookId)
+                .FirstOrDefault();
+
+            Category c1 = _db.Categories
+                .Where(a => a.Name ==product.category.Name)
+                .FirstOrDefault();
+
+            if(p1!=null)
+            {
+                p1.BookTitle = product.BookTitle;
+                p1.ISBN = product.ISBN;
+                p1.Description= product.Description;
+                p1.Author= product.Author;
+                p1.Price = product.Price;
+                p1.category = c1;
+                p1.CategoryId = c1.CategoryId;
+                _db.SaveChanges();
+            }
+            //Console.WriteLine(product);
+            //_db.Products.Update(product);
+            //_db.SaveChanges();
             return RedirectToAction("Index");
         }
         public IActionResult Delete(int id)
