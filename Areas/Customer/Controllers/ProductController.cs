045a770bc089cb0141dbaa5540e46475ef546a98
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-09 12:10:12 +0530

    Bug Resolved : Pop Up will Delete The False Product And Category

--- Areas/Customer/Controllers/ProductController.cs
+++ Areas/Customer/Controllers/ProductController.cs
@@ -22,43 +22,58 @@ namespace ECommerce.Areas.Customer.Controllers
             return View(data1);
         }
         public IActionResult Create() {
+            List<SelectListItem> list = _db.Categories.Select(e => new SelectListItem
+            {
+                Text = e.Name,
+                Value = e.CategoryId.ToString()
+            }).ToList();
+            ViewBag.categoryList = list;
             return View();
         }
         [HttpPost]
         public IActionResult Create(Product product)
         {
+            Category c1 = _db.Categories
+                .Where(c => c.CategoryId == (int)product.CategoryId)
+                .FirstOrDefault();
+            product.category = c1;
             _db.Products.Add(product);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
         public IActionResult Edit(int id) {
-
+            List<SelectListItem> list = _db.Categories.Select(e => new SelectListItem
+            { 
+                Text = e.Name,
+                Value = e.CategoryId.ToString()
+            }).ToList();
             Product data = _db.Products.Include(_ => _.category).Where(p=>p.BookId==id).FirstOrDefault();
             Console.WriteLine(data);
+            ViewBag.categoryList = list;
             return View("Update",data);
         }
 
         [HttpPost]
         public IActionResult Edit(Product product)
         {
-
+            Category c1 = _db.Categories
+                .Where(c => c.CategoryId == (int)product.CategoryId)
+                .FirstOrDefault();
             Product p1 = _db.Products.Include(_ => _.category)
                 .Where(_ => _.BookId == product.BookId)
                 .FirstOrDefault();
 
-            Category c1 = _db.Categories
-                .Where(a => a.Name ==product.category.Name)
-                .FirstOrDefault();
 
-            if(p1!=null)
+
+            if (p1 != null)
             {
                 p1.BookTitle = product.BookTitle;
                 p1.ISBN = product.ISBN;
-                p1.Description= product.Description;
-                p1.Author= product.Author;
+                p1.Description = product.Description;
+                p1.Author = product.Author;
                 p1.Price = product.Price;
                 p1.category = c1;
-                p1.CategoryId = c1.CategoryId;
+                p1.CategoryId = product.CategoryId;
                 _db.SaveChanges();
             }
             //Console.WriteLine(product);
